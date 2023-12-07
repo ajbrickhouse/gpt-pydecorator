4faf304f68a460f53bf5686f8dfe9112e2d9297a
Tony Brinkhuis <ajbrinkhuis@gmail.com>
2023-12-07 05:33:31 -0700

    changed

--- chatbot.py
+++ chatbot.py
@@ -8,17 +8,19 @@ import json
 from dotenv import load_dotenv
 import openpyxl
 from datetime import datetime
+import discord
+import asyncio
 
 # Load the API key from .env file
 load_dotenv()
-openai.api_key = "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"
-
-# Convert datetime object to string
-def default(o, *args):
+def default(o):
     if isinstance(o, datetime):
         return o.__str__()
+    return str(o)  # Ensure content is always a string
 
 json.JSONEncoder.default = default
+
+openai.api_key = "sk-FdCrfAucoEQk9J0cdXqxuT3BlbkFJM1HsMjeNtK9vnsnzgztX"
 
 @openaifunc
 def keyword_search_excel(keyword) -> dict:
@@ -84,61 +86,67 @@ def send_message(message, messages):
     messages.append(response["choices"][0]["message"])
     return messages
 
+class MyClient(discord.Client):
+    async def start_bot(self):
+        while True:
+            try:
+                await self.start(os.getenv("DISCORD_TOKEN"))
+            except discord.errors.ConnectionClosed as e:
+                print(f"Connection closed: {e}, attempting to reconnect in 5 seconds.")
+                await asyncio.sleep(5)  # wait before reconnecting
+            except Exception as e:
+                print(f"Unexpected error: {e}")
+                break  # Exit the loop if the error is not related to connection
+
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return  # Avoid responding to the bot's own messages
+
+        messages = []  # Initialize messages as an empty list
+        messages = send_message({"role": "user", "content": message.content}, messages)
+        
+        # Get ChatGPT response
+        chatgpt_response = messages[-1]["content"]
+
+        # If ChatGPT response is a function call, call the function
+        if messages[-1].get("function_call"):
+            function_name = messages[-1]["function_call"]["name"]
+            arguments = json.loads(messages[-1]["function_call"]["arguments"])
+
+            if function_name in globals() and callable(globals()[function_name]):
+                if function_name == "keyword_search_excel":
+                    keyword = arguments.get("keyword")
+                    if keyword:
+                        function_response = globals()[function_name](keyword)
+                    else:
+                        print("Keyword argument is missing for keyword_search_excel")
+                        await message.channel.send("Error: Keyword argument is missing.")
+                        return  # Exit the function early
+                else:
+                    function_response = globals()[function_name](**arguments)
+
+                # Send function result back to ChatGPT
+                messages = send_message(
+                    {
+                        "role": "function",
+                        "name": function_name,
+                        "content": function_response,
+                    },
+                    messages,
+                )
+
+            # Get the final response from ChatGPT
+            final_response = messages[-1]["content"]
+
+            # Send final response to Discord
+            await message.channel.send(final_response)
 
 
-# MAIN FUNCTION
-def run_conversation(prompt, messages=[]):
-    # add user prompt to chatgpt messages
-    messages = send_message({"role": "user", "content": prompt}, messages)
+intents = discord.Intents.default()
+intents.message_content = True
 
-    # get chatgpt response
-    message = messages[-1]
-
-    # loop until project is finished
-    while True:
-        if message.get("function_call"):
-            # get function name and arguments
-            function_name = message["function_call"]["name"]
-            arguments = json.loads(message["function_call"]["arguments"])
-
-            # call function dangerously
-            if function_name == "get_current_weather":
-                function_response = globals()[function_name](arguments.get("location"), arguments.get("country"))
-            else:
-                function_response = globals()[function_name](**arguments)
-
-            # send function result to chatgpt
-            messages = send_message(
-                {
-                    "role": "function",
-                    "name": function_name,
-                    "content": function_response,
-                },
-                messages,
-            )
-        else:
-            # if chatgpt doesn't respond with a function call, ask user for input
-            print("ChatGPT: " + message["content"])
-
-            user_message = input("You: ")
-
-            # send user message to chatgpt
-            messages = send_message(
-                {
-                    "role": "user",
-                    "content": user_message,
-                },
-                messages,
-            )
-
-        # save last response for the while loop
-        message = messages[-1]
-
-# ASK FOR PROMPT
-print(
-    "Go ahead, ask for the weather, a YouTube channel recommendation or to calculate the length of a string!"
-)
-prompt = input("You: ")
-
-# RUN CONVERSATION
-run_conversation(prompt)
+client = MyClient(intents=intents)
+asyncio.run(client.start_bot())
